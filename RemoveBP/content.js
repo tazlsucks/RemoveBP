6b6de42dd7e3755d0b5c247f01c4bd2ab8bd44be
tazl <82299965+tazlsucks@users.noreply.github.com>
2023-12-03 18:05:15 -0500

    Delete RemoveBP directory

--- RemoveBP/content.js
+++
@@ -1,5 +0,0 @@
-    if (window.location.href.includes("/?bp")) {
-      try {
-        const newUrl = window.location.href.replace(/\/\?bp.*/, "");
-        window.location.href = newUrl;
-      } catch{}};
